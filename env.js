fbcf64ab834356d76e9876e60660ff6cf2c650fe
Plang dakon <34747511+Dakony@users.noreply.github.com>
2019-05-05 23:59:52 +0100

    Add files via upload

---
+++ env.js
@@ -0,0 +1,14 @@
+export const dialogflowConfig = {
+  type: "service_account",
+  project_id: "faq-bot-4d420",
+  private_key_id: "3a78898cd93721a6ac7c33d98a2c71d1b514dfd6",
+  private_key:
+    "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCxDTQh3A49rBJG\nxk8vBZOyVjab2kMmbsSZ76b+kp/E5QBAnF6GBtiZwy74oTV61+SkLpHrbbWw3RB9\n8tfIkgC7fnm+7BxCzZXhh3M4OmNWrCQ7bkZQK0AuxeAoUbXTiZamxtDaFiHyKHgC\nLjt5gvkmhncsRWyyNdlLw3MFfgSGcM3WJnFH5Cml+tGvTk0NbdWnfqIFDwxCtERf\nCJk/2caisaq/h5N/AHIbqOMt4uoVmmUqCH2ytma6xsWOqKTMcE5x/xeb2l2tbHKg\nwwjHwl5V+W1Tmvd1kFK0bydjexMkdcJD9trIrkSJ7PvKADNK5YOWMplqJ/Ua2KYu\noibrooYdAgMBAAECggEAQgpwRy77euKbwJ2I/LuiT3OcE/9Sa9wGZnr/eda8VKes\ns+z/zxNvqM8mzqPnPGaaSOl8EWqPSqTy2WADQDPBpS40gFQ26G4h+YlaH3+b/UDh\nlO95t76iRrAVgESWQ4z/Tj87uRVnXRneRcXz9ku13rpqam+M715pqrqBRWuviWuc\ncEv5o4Sb3+74r5P7z6ZxnYnnFi1SkQJuEe+3jFn5OAFkDvPPCOM7tp4UJe7EvaV9\n1SXkVCEAQY8FPUe6gSOTIWH+FGeHJveoxiQYVSXaHWjdxQ5gvWf4sEkm1RZ/WHaf\ns1+EHIUzU5BS6JeHRe8gh1Q3KEnD3Q2ZOKNEZtB5WwKBgQDVrGrVbXlmWqoSMkYX\npRNYbsexK4B2CYK5JRo3+NI27IjP5FFUGwCXrnpccAXpVO2RR5gEEs5SC1rboppy\n3SvPdC8r53I6Hrmi8T1cWDLunPHfefzG4Gi0QI7asicp7q6rKAPBaUjTQPkIyYpx\nWqQnKBjKN42/NPBN4Llfe4MtpwKBgQDUH6d4ppT49Xx79RmkERcvsLNU8WiV6o6o\nQcA+mmDOLHVgCgI3XI+xsN8YuZzHp0hbXOZYlZqHkhn8bGdrzPL3capqJz1JJyaN\ntl9VuQl+RshMt/QIfI70ANGnIkyaHl4D1cHFBfhhcAoDvMSSxZJFLp2GSM9Fn8Ua\nVQkqBetOmwKBgFq7dT4d14AsHa3OZU8jDLzvhaJo+4qj7TubUq/T28fw1wOYSySw\ntqayM0lPVGNJafvX7+hC6BOUuMctG/KMkk7sYh99j1a4t1FGqSyuUypZwKeJ0FnO\n7wcjpsWdAt5eeRM60aVuhLsYCue9VSlfOPdbonMz9d/speIb+/tcok5fAoGBAI8m\nSXtXV6Nm7zor3sDks7QAWBVT0NOS9fDAZn8Y6YlZYWCNgI9hzKsKhJQH9rCZyG+3\nks9nLmCdDSJW+bg7GCA6udlnX6l1XUQ3NcEA3VI1bEixv2LWxBYQU2up7u323lVS\nNLDEZalD1c5N3o55/ErpuW58xjvjJT2u8z9DvVdPAoGBALvy8ODfjccjM6rsuiid\np8OX3lOocd1Lmvcr5z1zXC68uX7jPY7I9z8MiPncUzXYa/GpLdK21Y0VtwmnvxUJ\n194oYcxJIzvSAjZNpl8UyUIIp1IO3zbyuJHsG6zFYsj3qtf4ZrUWdfuWckEMuOQi\nFAaAaqeYznZGi/dRhfmScdW7\n-----END PRIVATE KEY-----\n",
+  client_email: "dialogflow-qrjjqp@faq-bot-4d420.iam.gserviceaccount.com",
+  client_id: "115583143944682364711",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/dialogflow-qrjjqp%40faq-bot-4d420.iam.gserviceaccount.com"
+};
